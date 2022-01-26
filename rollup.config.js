0856bc4976ffff32969b2792f7114b25ae41994d
Swordword <massivejohn123@gmail.com>
2022-01-26 18:32:44 +0800

    feat: rollup init

---
+++ rollup.config.js
@@ -0,0 +1,63 @@
+import babel from '@rollup/plugin-babel'
+import typescript from 'rollup-plugin-typescript2'
+import commonjs from '@rollup/plugin-commonjs'
+import external from 'rollup-plugin-peer-deps-external'
+import postcss from 'rollup-plugin-postcss'
+import resolve from '@rollup/plugin-node-resolve'
+import url from '@rollup/plugin-url'
+import { terser } from 'rollup-plugin-terser'
+import { defineConfig } from 'rollup'
+import typescriptEngine from 'typescript'
+
+import { DEFAULT_EXTENSIONS } from '@babel/core'
+import pkg from './package.json'
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+export default defineConfig([
+  {
+    input: './src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        exports: 'named',
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+        exports: 'named',
+      },
+    ],
+    plugins: [
+      postcss({
+        plugins: [],
+        minimize: true,
+      }),
+      external({
+        includeDependencies: true,
+      }),
+      typescript({
+        typescript: typescriptEngine,
+        include: ['*.js+(|x)', '**/*.js+(|x)'],
+        exclude: [
+          'coverage',
+          'config',
+          'dist',
+          'node_modules/**',
+          '*.test.{js+(|x), ts+(|x)}',
+          '**/*.test.{js+(|x), ts+(|x)}',
+        ],
+      }),
+      commonjs(),
+      babel({
+        extensions: [...DEFAULT_EXTENSIONS, '.ts', 'tsx'],
+        babelHelpers: isDevelopment ? 'runtime' : 'bundled',
+        exclude: /node_modules/,
+      }),
+      url(),
+      resolve(),
+      terser(),
+    ],
+  },
+])
