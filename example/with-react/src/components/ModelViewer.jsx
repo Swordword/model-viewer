3d16f568913f0cf7879f1c573ae11e6ea3505478
Swordword <massivejohn123@gmail.com>
2022-01-28 17:38:58 +0800

    feat: 模型基本展示

---
+++ example/with-react/src/components/ModelViewer.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useRef } from 'react'
+import { Foo } from 'model-viewer'
+import './index.css'
+
+const ModelViewer = () => {
+  const ref = useRef()
+  useEffect(() => {
+    const foo = new Foo({
+      dom: ref.current,
+      type:'glb',
+      file:'http://r6ev1zfxk.hn-bkt.clouddn.com/%E5%B0%8F%E7%94%BB%E5%AE%B6%E5%88%86%E7%BB%847.glb'
+    })
+    foo.load()
+  }, [])
+  return (
+    <div>
+      hello
+      <canvas className='container' ref={ref}></canvas>
+    </div>
+  )
+}
+
+export default ModelViewer
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,81 @@
-function foo() {
-  return 'hello world'
+import * as THREE from 'three'
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+
+const onLoad = () => {}
+const onProgress = () => {}
+const onError = (error: Error) => {
+  console.error(error.message)
 }
 
-export { foo }
+class Foo {
+  dom: any
+  type: any
+  file: any
+  scene: new THREE.Scene()
+  canvas: any
+  renderer: any
+  container: any
+  camera: any
+  constructor(config: any) {
+    const { dom, type, file } = config
+    const canvas = dom
+    this.canvas = canvas
+    this.container = canvas
+    this.type = type
+    this.file = file
+    this.init()
+  }
+  init() {
+    // const scene = new THREE.Scene()
+    // this.scene = scene
+    const renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+      // 执行抗锯齿
+      antialias: true,
+      // 包含透明度
+      alpha: true,
+      // 设置为可缓存
+      preserveDrawingBuffer: true,
+    })
+    this.renderer = renderer
+  }
+  initCamera() {
+    const camera = new THREE.PerspectiveCamera(
+      45,
+      this.container.offsetWidth / this.container.offsetHeight,
+      0.1,
+      1000
+    )
+    camera.position.set(0, 10, 20)
+    this.camera = camera
+  }
+  initLight() {
+    const ambientLight = new THREE.AmbientLight('#ffffff', 1)
+    this.scene.add(ambientLight)
+  }
+  render() {
+    this.renderer.render(this.scene, this.camera)
+  }
+  load() {
+    const loader = new GLTFLoader()
+    loader.load(
+      this.file,
+      (gltf) => {
+        console.log('gltf', gltf)
+        this.scene.add(gltf.scene)
+        // 初始化相机
+        this.initCamera()
+        // 初始化光源
+        this.initLight()
+        this.render()
+      },
+      onProgress,
+      (error) => {
+        console.error(error)
+      }
+    )
+  }
+}
+
+export { Foo }
